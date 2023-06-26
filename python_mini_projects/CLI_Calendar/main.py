cf4fe49682b86644841d39875a533711facb44b4
N34R <francisco.olmos.99@gmail.com>
2023-06-26 02:14:00 -0300

    update main so the app saves the data into a json file

--- python_mini_projects/CLI_Calendar/main.py
+++ python_mini_projects/CLI_Calendar/main.py
@@ -1,71 +1,92 @@
 """
 Calendar in the command line App
 """
+
+import json
 from time import sleep, strftime
 
 user_first_name = "Francisco"
 
 calendar = {}
 
+def load_calendar():
+    try:
+        with open("events.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_calendar():
+    with open("events.json", "w") as file:
+        json.dump(calendar, file)
+
 def welcome():
-  print('welcome {} to your calendar'.format(user_first_name))
-  print ("Calendar starting...")
-  sleep(1)
-  print ("Today is: " + strftime("%A %B %d, %Y"))
-  print (strftime("%H:%M:%S"))
+    print('Welcome, {}!'.format(user_first_name))
+    print("Calendar starting...")
+    sleep(1)
+    print("Today is: " + strftime("%A %B %d, %Y"))
+    print(strftime("%H:%M:%S"))
 
 def start_calendar():
-  welcome()
-  start = True
-  while start:
-    user_choice = input("A to Add, U to Update, V to View, D to Delete, X to Exit: ")
-    user_choice = user_choice.upper()
+    global calendar
+    calendar = load_calendar()
+    welcome()
+    start = True
+    while start:
+        user_choice = input("A to Add, U to Update, V to View, D to Delete, X to Exit: ")
+        user_choice = user_choice.upper()
 
-    if user_choice == "V":
-      if len(calendar.keys()) < 1:
-        print('calendar empty')
-      else:
-        print(calendar)
+        if user_choice == "V":
+            if len(calendar.keys()) < 1:
+                print('Calendar is empty.')
+            else:
+                print(calendar)
 
-    elif user_choice == "U":
-      date = input("What date? ")
-      update = input("Enter the update: ")
-      calendar[date] = update
-      print("update sucesfull")
-      print(calendar)
+        elif user_choice == "U":
+            date = input("What date? ")
+            update = input("Enter the update: ")
+            calendar[date] = update
+            save_calendar()
+            print("Update successful.")
+            print(calendar)
 
-    elif user_choice == "A":
-      event = input("Enter event: ")
-      date = input("Enter date (MM/DD/YYYY): ")
-      if (len(date) > 10) or (int(date[6:]) < int(strftime("%Y"))):
-        print("you have entered an invalid date")
-        try_again = input("Try Again? Y for Yes, N for No: ")
-        try_again = try_again.upper()
-        if try_again == 'Y':
-          continue
+        elif user_choice == "A":
+            event = input("Enter event: ")
+            date = input("Enter date (MM/DD/YYYY): ")
+            if len(date) > 10 or int(date[6:]) < int(strftime("%Y")):
+                print("You have entered an invalid date.")
+                try_again = input("Try Again? Y for Yes, N for No: ")
+                try_again = try_again.upper()
+                if try_again == 'Y':
+                    continue
+                else:
+                    start = False
+            else:
+                calendar[date] = event
+                save_calendar()
+                print("Event added successfully.")
+                print(calendar)
+
+        elif user_choice == "D":
+            if len(calendar.keys()) < 1:
+                print('Calendar is empty.')
+            else:
+                event = input("What event?")
+                for date in calendar.keys():
+                    if event == calendar[date]:
+                        del calendar[date]
+                        save_calendar()
+                        print("The event was successfully deleted.")
+                        break
+                else:
+                    print('There is no event with that name in the calendar.')
+
+        elif user_choice == "X":
+            start = False
+
         else:
-          start = False   
-      else:
-        calendar[date] = event 
-        print("update sucesfull")
-        print(calendar)  
+            print('')
+            start = False
 
-    elif user_choice == "D":
-      if len(calendar.keys()) < 1:
-        print('calendar empty')
-      else:
-        event = input("What event?")
-        for date in calendar.keys():
-          if event == calendar[date]:
-            del (calendar[date])
-            print("the event was succesfully deleted")
-          else:
-            print('There is no event with that name in the calendar')
-    elif user_choice == "X":
-      start = False
-      
-    else:
-      print('')
-      start = False
+start_calendar()
 
-start_calendar()   
